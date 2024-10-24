dbaab14bb7a836749cb44474b25602b9dc862fce
raflidewanto <rafli.dewanto@smsfinance.co.id>
2024-10-24 11:36:07 +0700

    fix dark style on toggle button

--- components/layout/ThemeToggle/theme-toggle.tsx
+++ components/layout/ThemeToggle/theme-toggle.tsx
@@ -9,7 +9,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
+
 type CompProps = {};
+
 export default function ThemeToggle({}: CompProps) {
   const { setTheme } = useTheme();
   return (
@@ -18,14 +20,14 @@ export default function ThemeToggle({}: CompProps) {
         <Button
           variant="outline"
           size="icon"
-          className="border border-zinc-50 bg-white dark:border-zinc-800 dark:bg-zinc-700"
+          className="border border-zinc-50 bg-white dark:border-zinc-800 dark:bg-zinc-900 dark:hover:bg-zinc-800"
         >
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-zinc-500" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="dark:bg-zinc-900 dark:text-white">
         <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
